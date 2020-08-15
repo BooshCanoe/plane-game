6fab61ef75bae22f90651ef534f7b7112d2009a4
BooshCanoe <dylan.jdmatthews@gmail.com>
2020-08-15 11:48:52 -0700

    Got movement and rotation for plane working - Some jitters

--- Plane Game/Assets/Scripts/PlayerMovementController.cs
+++ Plane Game/Assets/Scripts/PlayerMovementController.cs
@@ -7,9 +7,9 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 100;
     [SerializeField]
-    private float RotationSpeed = 50;
+    private float rotationSpeed = 50;
     [SerializeField]
-    private float rotationSpeedReduction = 2;
+    private float maxSpeed;
 
     private new Rigidbody2D rigidbody2D;
 
@@ -20,16 +20,35 @@ public class PlayerMovementController : MonoBehaviour
 
     void Update()
     {
-        var rotateSpeed = Input.GetAxis("Horizontal") * RotationSpeed * Time.deltaTime;
+        ClampMaxSpeed();
 
         if (Input.GetKey(KeyCode.W))
         {
-            rigidbody2D.velocity = (Vector2.up * moveSpeed * Time.deltaTime);
+            Boost();
         }
 
-        if (Input.GetAxis("Horiztonal") > 0)
-        {
-            //rigidbody2D.rotation
-        }
+        if (Input.GetKey(KeyCode.A))
+            Rotate(rotationSpeed * Time.fixedDeltaTime);
+        else if (Input.GetKey(KeyCode.D))
+            Rotate(rotationSpeed * Time.fixedDeltaTime * -1);
+    }
+
+    private void Rotate(float rotationAmount)
+    {
+        transform.Rotate(0, 0, rotationAmount);
+    }
+
+    private void Boost()
+    {
+        var direction = transform.up;
+        var accelerationSpeed = moveSpeed * Time.fixedDeltaTime;
+
+        rigidbody2D.velocity = direction * accelerationSpeed;
+    }
+
+    private void ClampMaxSpeed()
+    {
+        float x = Mathf.Clamp(rigidbody2D.velocity.x, - maxSpeed, maxSpeed);
+        float y = Mathf.Clamp(rigidbody2D.velocity.y, - maxSpeed, maxSpeed);
     }
 }
